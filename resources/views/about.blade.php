09f8236f5dc8ea3dc299fa64f42032f2c338a6ff
MiguelM <up202108851@up.pt>
2023-11-28 00:34:49 +0000

    about, contacts, welcome, login html

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+</head>
+<body>
+    <h1>About Me</h1>
+    <p>Hi! My (online) alias is CurlMike and this is my first web development related project. <br>
+        <br>
+        This is The Wired. If you have watched "Serial Experiments Lain", you will already know what this is all about. <br>
+        Based on the anime and especially on the PS1 game, this blog type of website will serve as a social network website. <br>
+        <br>
+        You can create diary entries, talk about what's on your mind and share your thoughts and problems, or simply tell how your day went. <br>
+        For now that is all, but as I get more familiar with this tech, maybe it will get better. <br>
+        <br>
+        Maybe one day, you'll even upload your conscience here, and leave your mortal body behind. :) <br>
+        <br>
+        CurlMike
+    </p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Welcome
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Static Pages
+Route::view('about', 'about')->name('about');
+Route::view('contacts', 'contacts')->name('contact');
+
+// Auth
+Route::controller(LoginController::class)->group(function () {
+    Route::get('login', 'index')->name('login.index');
+    Route::post('login', 'authenticate')->name('login.auth');
 });
