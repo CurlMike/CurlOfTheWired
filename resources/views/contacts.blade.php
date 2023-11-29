b6178f5eb171c82350e6e1bff25515646035a23e
MiguelM <up202108851@up.pt>
2023-11-29 01:35:52 +0000

    can extend layout now :fire:

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contacts</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>Contacts</h1>
     <p>Discord: curlmike <br>
         <a href="https://twitter.com/curlmike/">Twitter</a> <br>
         <a href="https://github.com/curlmike/">GitHub</a> <br>
     </p>
-</body>
-</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,35 @@
-<!DOCTYPE html>
-<html lang="en" class="h-screen flex items-center justify-center bg-black">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>The Wired</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
-</head>
-<body class="text-white font-sans text-center">
-    <img src="{{ asset('images/navi.gif') }}" alt="Navi" class="mx-auto mb-4">
+@extends('layouts.app')
 
-    <h1 class="text-4xl font-bold mb-4">Welcome to The Wired</h1>
-    <ul class="mb-4">
-        <li class="inline-block relative">
-            <a href="{{ url('/about') }}" class="text-white-300 hover:text-blue-500 mr-4">About</a>
-            <div class="h-full absolute top-0 right-0 bg-white w-0.5"></div>
-        </li>
-        <li class="inline-block relative">
-            <a href="{{ url('/contacts') }}" class="text-white-300 hover:text-blue-500 mr-4 ml-2">Contacts</a>
-            <div class="h-full absolute top-0 right-0 bg-white w-0.5"></div>
-        </li>
-        @guest
-        <li class="inline-block relative">
-            <a href="{{ route('login.index') }}" class="text-white-300 hover:text-blue-500 mr-4 ml-2">Log in</a>
-            <div class="h-full absolute top-0 right-0 bg-white w-0.5"></div>
-        </li>
-        <li class="inline-block relative">
-            <a href="{{ route('register.index') }}" class="text-white-300 hover:text-blue-500 ml-2">Create account</a>
-        </li>
-        @else
-        <li class="inline-block relative">
-            <a href="{{ route('login.logout') }}" class="text-white-300 hover:text-blue-500 ml-2">Log out</a>
-        </li>
-        @endguest
-    </ul>
-</body>
-</html>
+@section('content')
+    <div class="h-screen flex items-center justify-center bg-black">
+        <div class="text-white font-sans text-center">
+            <img src="{{ asset('images/navi.gif') }}" alt="Navi" class="mx-auto mb-4">
+
+            <h1 class="text-4xl font-bold mb-4">Welcome to The Wired</h1>
+            <ul class="mb-4">
+                <li class="inline-block relative">
+                    <a href="{{ url('/about') }}" class="text-white-300 hover:text-blue-500 mr-4">About</a>
+                    <div class="h-full absolute top-0 right-0 bg-white w-0.5"></div>
+                </li>
+                <li class="inline-block relative">
+                    <a href="{{ url('/contacts') }}" class="text-white-300 hover:text-blue-500 mr-4 ml-2">Contacts</a>
+                    <div class="h-full absolute top-0 right-0 bg-white w-0.5"></div>
+                </li>
+                @guest
+                <li class="inline-block relative">
+                    <a href="{{ route('login.index') }}" class="text-white-300 hover:text-blue-500 mr-4 ml-2">Log in</a>
+                    <div class="h-full absolute top-0 right-0 bg-white w-0.5"></div>
+                </li>
+                <li class="inline-block relative">
+                    <a href="{{ route('register.index') }}" class="text-white-300 hover:text-blue-500 ml-2">Create account</a>
+                </li>
+                @else
+                <li class="inline-block relative">
+                    <a href="{{ route('login.logout') }}" class="text-white-300 hover:text-blue-500 ml-2">Log out</a>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+@endsection
 
