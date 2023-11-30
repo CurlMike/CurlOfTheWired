70e2160dedee65da6358623b20692c680aad70d1
MiguelM <up202108851@up.pt>
2023-11-30 22:45:48 +0000

    folders changed, topbar sticks on top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,19 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>The Wired</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
-    <style>
-        html {
-            overflow:   scroll;
-        }
-
-        ::-webkit-scrollbar {
-            width: 0px;
-            display: none; /* make scrollbar dissapear */
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 </head>
 <body>
-    @yield('topbar')
-    @yield('content')
+    <div class="topbar-container fixed top-0 w-full z-10">
+        @yield('topbar')
+    </div>
+    <div class="content-container">
+        @yield('content')
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- resources/views/partials/_profile.blade.php
+++ resources/views/partials/_profile.blade.php
@@ -1,3 +1,6 @@
 @section('profile')
-    {{-- Todo --}}
+    <h2>{{$user->username}}</h2>
+    <h3>{{$user->name}}</h3>
+    <h4>{{$user->user_type}}</h4>
+    <p>{{$user->bio}}</p>
 @endsection
