7ecac71d56dacc6cba0101fc24e6547502bcf9ad
MiguelM <up202108851@up.pt>
2023-12-18 01:18:46 +0000

    fixing styles on profile :fire:

--- resources/views/partials/_entry.blade.php
+++ resources/views/partials/_entry.blade.php
@@ -2,10 +2,14 @@
     <div class="entry">
         <div class="flex items-center mb-4">
             <div class="flex justify-between mr-4">
-                <img src="{{ asset('storage/avatars/' . $user->avatar)}}" class="w-10 h-10 rounded-full">
+                <a href="{{ route('user.index', [ 'account_name' => $user->account_name ])}}">
+                    <img src="{{ asset('storage/avatars/' . $user->avatar)}}" class="w-10 h-10 rounded-full">
+                </a>
                 <div>
-                    <h3 class="text-lg font-semibold ml-2">{{ $user->username }}</h3>
-                    <h5 class="flex text-gray-400 -mt-2 ml-1.5"><span>@</span>{{ $user->account_name }}</h5>
+                    <a href="{{ route('user.index', [ 'account_name' => $user->account_name ])}}">
+                        <h3 class="hover:underline text-lg font-semibold ml-2">{{ $user->username }}</h3>
+                        <h5 class="flex text-gray-400 -mt-2 ml-1.5"><span>@</span>{{ $user->account_name }}</h5>
+                    </a>
                 </div>
             </div>
             <p class="text-gray-500 mx-auto">{{ $entry->created_at->diffForHumans() }}</p>
@@ -15,7 +19,7 @@
                 </a>
             </div>
         </div>
-        <h3 class="text-xl font-semibold mb-4">{{ $entry->title }}</h3>
+        <h3 class="text-xl font-semibold mb-3">{{ $entry->title }}</h3>
         <div class="entry-text">
             <p class="text-gray-300 mb-4">{{ $entry->content }}</p>
         </div>
--- resources/views/partials/_profile.blade.php
+++ resources/views/partials/_profile.blade.php
@@ -22,11 +22,25 @@
             <div class="flex flex-col items-start">
                 <h2 class="text-2xl font-bold">{{ $user->username }}</h2>
                 <h3 class="text-lg"><span>@</span>{{ $user->account_name }}</h3>
-                <h4 class="text-sm">{{ $user->user_type }}</h4>
+                <h4 class="text-sm mb-2">{{ $user->user_type }}</h4>
+                <div class="flex mb-2">
+                    <div class="flex mr-4">
+                        <p class="text-sm font-medium mr-1">10K</p>
+                        <p class="text-sm font-medium text-gray-600">Followers</p>
+                    </div>
+                    <div class="flex mr-4">
+                        <p class="text-sm font-medium mr-1">15</p>
+                        <p class="text-sm font-medium text-gray-600">Following</p>
+                    </div>
+                    <div class="flex">
+                        <p class="text-sm font-medium text-gray-600 mr-1">Joined on</p>
+                        <p class="text-sm font-medium">{{ $user->joined_at }}</p>
+                    </div>
+                </div>
                 <div class="border-b-2 border-white w-32"></div>
             </div>
         </div>
-        <div class="bio">
+        <div class="bio profile-details">
             <p class="text-gray-300">{{ $user->bio }}</p>
         </div>
         <hr class="w-96 h-1 mx-auto my-4 bg-white border-0 rounded md:my-6 dark:bg-gray-700" />
