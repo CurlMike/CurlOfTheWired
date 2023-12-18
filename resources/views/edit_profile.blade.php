14183f8b7c9634a4e2bf514da886424d7577565d
MiguelM <up202108851@up.pt>
2023-12-18 00:26:04 +0000

    fixing small typos and making entries look a bit better

--- resources/views/edit_profile.blade.php
+++ resources/views/edit_profile.blade.php
@@ -16,19 +16,19 @@
             <div class="mb-4">
                 <label for="username" class="block mb-2 font-medium text-white text-xl">Display name</label>
                 <input class="bg-transparent border boder-white p-2" id="username" value="{{ $user->username }}" type="text" name="username" placeholder="Display name">
-                <p class="text-gray-500">This is the name that appears on your profile.</p>
+                <p class="text-gray-500 mt-1">This is the name that appears on your profile.</p>
             </div>
             <div class="mb-4">
                 <label for="email" class="block mb-2 font-medium text-white text-xl">E-mail</label>
                 <input class= "bg-transparent border border-white p-2" id="email" value="{{ $user->email }}" type="email" name="email" placeholder="E-mail">
-                <p class="text-gray-500">This is the e-mail associated with your profile. <br>
+                <p class="text-gray-500 mt-1">This is the e-mail associated with your profile. <br>
                     Keep it updated in case you lose your password.
                 </p>
             </div>
             <div class="mb-4">
                 <label for="bio" class="block mb-2 font-medium text-white text-xl">Biography</label>
                 <textarea class="bg-transparent border border-white p-2 w-96 h-32" id="bio" name="bio" placeholder="Biography">{{$user->bio}}</textarea>
-                <p class="text-gray-500">This is your profile biography. You can use it to tell people more about your page.</p>
+                <p class="text-gray-500 mt-1">This is your profile biography. You can use it to tell people more about your page.</p>
             </div>
             <div class="mb-4">
                 <label for="private" class="block mb-2 font-medium text-white text-xl">Profile Privacy</label>
@@ -37,19 +37,19 @@
                     <label for="private" class="toggle-label block overflow-hidden h-6 rounded-full bg-transparent border-2 border-white cursor-pointe focus:outline-none transition-all duration-300 ease-in-out"></label>
                 </div>
                 <label for="private" class="text-white">Private Account</label>
-                <p class="text-gray-500">Public accounts are visible to anyone. Private accounts are only visible to followers. <br> Select the box if you wish to make your account private.</p>
+                <p class="text-gray-500 mt-1">Public accounts are visible to anyone. Private accounts are only visible to followers. <br> Select the box if you wish to make your account private.</p>
             </div>                       
             <div class="mb-4">
-                <label for="avatar" class="block mb-2 font-medium text-white text-xl">Profile Pircute</label>
+                <label for="avatar" class="block mb-2 font-medium text-white text-xl">Profile Picture</label>
                 <img src="{{ asset('storage/avatars/' . $user->avatar)}}" class="mx-auto block img-thumbnail w-32 h-32 mb-2" alt="Avatar">
                 <input type="file" class="ml-24 form-control" id="avatar" name="avatar">
-                <p class="text-gray-500">This is the profile picture that appears on your profile page.</p>
+                <p class="text-gray-500 mt-1">This is the profile picture that appears on your profile page.</p>
             </div>
             <div class="mb-4">
                 <label for="banner" class="block mb-2 font-medium text-white text-xl">Profile Banner</label>
                 <img src="{{ asset('storage/banners/' . $user->banner)}}" class="mx-auto block img-thumbnail w-128 h-32 mb-2" alt="Banner">
                 <input type="file" class="ml-24 form-control" id="banner" name="banner">
-                <p class="text-gray-500">This is the profile banner that appears on your profile page.</p>
+                <p class="text-gray-500 mt-1">This is the profile banner that appears on your profile page.</p>
             </div>
             <button class="white-border-btn" type="submit">Update Profile</button>
         </form>
--- resources/views/partials/_entry.blade.php
+++ resources/views/partials/_entry.blade.php
@@ -2,12 +2,23 @@
     <div class="entry">
         <div class="flex items-center mb-4">
             <div class="flex justify-between mr-4">
-                <img src="{{ asset('storage/avatars/' . $user->avatar)}}" class="w-8 h-8 rounded-full">
-                <h3 class="text-lg font-semibold ml-2">{{ $user->account_name }}</h3>
+                <img src="{{ asset('storage/avatars/' . $user->avatar)}}" class="w-10 h-10 rounded-full">
+                <div>
+                    <h3 class="text-lg font-semibold ml-2">{{ $user->username }}</h3>
+                    <h5 class="flex text-gray-400 -mt-2 ml-1.5"><span>@</span>{{ $user->account_name }}</h5>
+                </div>
             </div>
-            <p class="text-gray-500">{{ $entry->created_at->diffForHumans() }}</p>
+            <p class="text-gray-500 mx-auto">{{ $entry->created_at->diffForHumans() }}</p>
+            <div class="ml-auto icons">
+                <a href="" class="icon-link">
+                    <i class="fa-solid fa-bars text-xl mr-1"></i>
+                </a>
+            </div>
         </div>
-        <h3 class="text-xl font-semibold mb-2">{{ $entry->title }}</h3>
-        <p class="text-gray-300 mb-4">{{ $entry->content }}</p>
+        <h3 class="text-xl font-semibold mb-4">{{ $entry->title }}</h3>
+        <div class="entry-text">
+            <p class="text-gray-300 mb-4">{{ $entry->content }}</p>
+        </div>
+        <hr/>
     </div>
 </div>
